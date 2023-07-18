06706c0e66c25d4cfd7f293c87fac825758f2d74
Martin Doherty <martin.doherty@kainos.com>
2023-07-18 14:36:29 +0100

    added integration test

---
+++ src/test/java/com/kainos/ea/integration/JobRolesIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.kainos.ea.integration;
+
+import com.kainos.ea.WebServiceApplication;
+import com.kainos.ea.WebServiceConfiguration;
+import com.kainos.ea.model.JobRole;
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import java.util.List;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class JobRolesIntegrationTest {
+
+    static final DropwizardAppExtension<WebServiceConfiguration> APP = new DropwizardAppExtension<>(
+            WebServiceApplication.class, null,
+            new ResourceConfigurationSourceProvider()
+    );
+
+    @Test
+    void getEmployees_shouldReturnListOfEmployees() {
+        List<JobRole> response = APP.client().target("http://localhost:8080/api/job-roles")
+                .request()
+                .get(List.class);
+
+        Assertions.assertTrue(response.size() > 0);
+    }
+
+
+}
