e396201246299a0dee5a46b2caab581ee33e61d2
stuferg <stuart.ferguson@kainos.com>
2023-07-21 12:01:57 +0100

    adding SQL scripts

---
+++ SQLScripts/US005-test-data.sql
@@ -0,0 +1,50 @@
+USE Team3_Test_IeuanB;
+
+	INSERT INTO Bands(band_name)
+    VALUES ("Trainee");
+    
+    INSERT INTO Bands(band_name)
+    VALUES ("Associate");
+    
+    INSERT INTO Bands(band_name)
+    VALUES ("Senior Associate");
+    
+    INSERT INTO Bands(band_name)
+    VALUES ("Consultant");
+    
+    INSERT INTO Bands(band_name)
+    VALUES ("Manager");
+    
+    INSERT INTO Competencies(name, description)
+    VALUES ("Perspnal Performance", "Effective Leadership require individuals to do stuff");
+    
+    INSERT INTO Competencies(name, description)
+    VALUES ("Working with Others", "Effective Leadership require individuals to get along");
+    
+    INSERT INTO Competencies(name, description)
+    VALUES ("Setting Direction, Development and Accountability", "Effective Leadership require individuals to contribute");
+    
+    INSERT INTO Competencies(name, description)
+    VALUES ("Supporting and Delivering the Strategy", "Effective Leadership require support, contribution, and delivery");
+    
+    INSERT INTO Competencies(name, description)
+    VALUES ("Commerciablity and Risk", "Effective Leadership require individuals to demonstrate effectivness");
+    
+    INSERT INTO Competencies(name, description)
+    VALUES ("Communicating and Influence", "Effective Leadership require individuals to be exceptional");
+    
+    
+    INSERT INTO Band_Competencies(band_id, competency_id)
+    VALUES (1, 3);
+    
+    INSERT INTO Band_Competencies(band_id, competency_id)
+    VALUES (2, 4);
+    
+    INSERT INTO Band_Competencies(band_id, competency_id)
+    VALUES (3, 5);
+    
+    INSERT INTO Band_Competencies(band_id, competency_id)
+    VALUES (4, 1);
+    
+    INSERT INTO Band_Competencies(band_id, competency_id)
+    VALUES (5, 2);
