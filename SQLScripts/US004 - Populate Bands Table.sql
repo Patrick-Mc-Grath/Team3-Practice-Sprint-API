5b7099310f38d319828dd39c3527e87d4d7827f4
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-07-21 13:53:23 +0100

    Added sql to populate Role_Bands table

--- SQLScripts/US004 - Populate Bands Table.sql
+++ SQLScripts/US004 - Populate Bands Table.sql
@@ -1,8 +1,8 @@
-INSERT INTO TABLE Bands(band_name) VALUES('Trainee');
-INSERT INTO TABLE Bands(band_name) VALUES('Apprentice');
-INSERT INTO TABLE Bands(band_name) VALUES('Associate');
-INSERT INTO TABLE Bands(band_name) VALUES('Senior Associate');
-INSERT INTO TABLE Bands(band_name) VALUES('Consultant');
-INSERT INTO TABLE Bands(band_name) VALUES('Manager');
-INSERT INTO TABLE Bands(band_name) VALUES('Principal');
-INSERT INTO TABLE Bands(band_name) VALUES('Leadership');
+INSERT INTO `Bands`(`band_name`) VALUES('Trainee');
+INSERT INTO `Bands`(`band_name`) VALUES('Apprentice');
+INSERT INTO `Bands`(`band_name`) VALUES('Associate');
+INSERT INTO `Bands`(`band_name`) VALUES('Senior Associate');
+INSERT INTO `Bands`(`band_name`) VALUES('Consultant');
+INSERT INTO `Bands`(`band_name`) VALUES('Manager');
+INSERT INTO `Bands`(`band_name`) VALUES('Principal');
+INSERT INTO `Bands`(`band_name`) VALUES('Leadership');
---
+++ SQLScripts/US004 - Populate Role_Bands Table.sql
@@ -0,0 +1,2 @@
+INSERT INTO `Role_Bands`(`role_id`, `band_id`) VALUES(1, 1);
+INSERT INTO `Role_Bands`(`role_id`, `band_id`) VALUES(2, 2);
