bafb03d0a6c92bc140888cb35e91c59006ff4841
Martin Doherty <martin.doherty@kainos.com>
2023-07-25 14:31:54 +0100

    u

--- SQLScripts/US001-JobRole.sql
+++ SQLScripts/US001-JobRole.sql
@@ -1,7 +1,10 @@
-CREATE TABLE IF NOT EXISTS `Job_Roles` (
-                             `role_id` smallint NOT NULL AUTO_INCREMENT,
-                             `role_title` varchar(50) NOT NULL,
-                             `spec_link` varchar(1000) NOT NULL,
-                             `spec_summary` varchar(1000) NOT NULL,
-                             PRIMARY KEY (`role_id`)
-);
+
+
+CREATE TABLE IF NOT exists `Job_Roles` (
+    `role_id` smallint NOT NULL AUTO_INCREMENT,
+    `role_title` varchar(50) NOT NULL,
+    `job_family_id` smallint DEFAULT NULL,
+    PRIMARY KEY (`role_id`),
+    KEY `FK1_job_family_id` (`job_family_id`),
+    CONSTRAINT `FK1_job_family_id` FOREIGN KEY (`job_family_id`) REFERENCES `Job_Families` (`job_family_id`)
+)
