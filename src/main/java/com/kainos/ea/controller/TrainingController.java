67d93aa26c87298c781601e17937f23b3d3eed06
ieuanbyers <137309450+ieuanbyers@users.noreply.github.com>
2023-07-31 10:11:51 +0100

    Update TrainingController.java

--- src/main/java/com/kainos/ea/controller/TrainingController.java
+++ src/main/java/com/kainos/ea/controller/TrainingController.java
@@ -17,7 +17,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
 
-@Api("Training")
+@Api("Backend API Training")
 @Path("/api")
 public class TrainingController {
 
