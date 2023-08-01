5eb1ccb689a4bd8fa7822d7af53c798e555d9acf
stuferg <stuart.ferguson@kainos.com>
2023-08-01 11:04:38 +0100

    Changing API name

--- src/main/java/com/kainos/ea/controller/CompetencyController.java
+++ src/main/java/com/kainos/ea/controller/CompetencyController.java
@@ -14,7 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Api("Engineering Academy Dropwizard Competency API")
+@Api("API Hack Street Boys")
 @Path("/api")
 public class CompetencyController {
 
