92ae55bbe26f8c35f16acd143b961b9c778a4c4e
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-08-03 10:38:45 +0100

    Minor changes to capability script and integration test

--- src/test/java/com/kainos/ea/integration/JobRolesIntegrationTest.java
+++ src/test/java/com/kainos/ea/integration/JobRolesIntegrationTest.java
@@ -50,7 +50,7 @@ public class JobRolesIntegrationTest {
         
         Assertions.assertTrue(jobRoles.size() > 0);
 
-        Assertions.assertEquals(29, pojos.get(1).getRoleId());
+        Assertions.assertEquals(1, pojos.get(1).getRoleId());
 
         Assertions.assertEquals("Software Engineer", pojos.get(1).getRoleTitle());
 
